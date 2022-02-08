cdcef2ca6ddf9af4cf71868347f4e3703f8f92ee
Burak Dağlı <omerburakdagli@gmail.com>
2022-02-08 18:44:36 +0300

    Django Veteriner App(Home, Models)

---
+++ vet_website/models.py
@@ -0,0 +1,27 @@
+from pydoc import describe
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+# Create your models here.
+class Pet(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    description = models.TextField()
+    type = models.CharField(max_length=100)
+    genus = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Owner(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=100)
+    address = models.TextField()
+    def __str__(self):
+        return self.user
+
+class PetOwnerMatching(models.Model):
+    ownerid = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    petid = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.id
+    
